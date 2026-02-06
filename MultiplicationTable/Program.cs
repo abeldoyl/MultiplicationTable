d0e0030f897d1455abb9500bcdcf5e6fdc7abc17
abeldoyl <abeldoyle71@gmail.com>
2026-02-05 19:29:07 -0700

    Add project files.

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                for (int column = 0; column < 10; column++)
+                {
+                    Console.Write((row * column).ToString().PadLeft(3));
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
